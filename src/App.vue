1b5cc843f3943b290780c67ccf61563438fa85db
chris-am <c.aranguizm@outlook.com>
2022-06-21 12:02:34 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import AboutPageVue from './modules/pokemon/pages/AboutPage.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { 
+    AboutPageVue    
   }
 }
 </script>
