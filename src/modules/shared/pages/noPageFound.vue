57f3e9e601ac585fbec7443900b4679e4a5ace47
chris-am <c.aranguizm@outlook.com>
2022-06-24 08:47:04 -0400

    creating and declaring 404 page view

---
+++ src/modules/shared/pages/noPageFound.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1> 404 </h1>
+    <h3> The Page you are looking for does not exists </h3>
+</template>
--- src/router/router.js
+++ src/router/router.js
@@ -3,12 +3,13 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import ListPage from "../modules/pokemon/pages/ListPage";
 import PokemonPage from "../modules/pokemon/pages/PokemonPage";
 import AboutPage from "../modules/pokemon/pages/AboutPage";
-
+import NoPageFound from "../modules/shared/pages/noPageFound"
 
 const routes = [
     { path: '/', component: ListPage },
     { path: '/about', component: AboutPage },
-    { path: '/id', component: PokemonPage }
+    { path: '/id', component: PokemonPage },
+    { path: '/:pathMatch(.*)*', component: NoPageFound }
 ]
 
 const router = createRouter({
